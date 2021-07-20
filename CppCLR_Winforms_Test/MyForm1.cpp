edbf390e2771ca3820b5d9b99b613a60ccc72abd
nancy <nancy@jettech.com.tw>
2021-07-20 18:11:57 +0800

    form2 copy

--- CppCLR_Winforms_Test/MyForm1.cpp
+++ CppCLR_Winforms_Test/MyForm1.cpp
@@ -2,15 +2,5 @@
 #include "MyForm1.h"
 
 
-//using namespace System::Windows::Forms;
-//using namespace CppCLR_Winforms_Test;
-//
-//[STAThread]
-//int main(array<System::String^> ^args){
-//	Application::EnableVisualStyles();
-//	Application::SetCompatibleTextRenderingDefault(false);
-//	Application::Run(gcnew MyForm1());
-//	return 0;
-//}
 
 
